9c9d7d782a3924b7b4c4a44e925ed21139cf9b90
Angela Zhang <angelazhangyr@gmail.com>
2020-08-17 19:14:23 -0400

    Draft 1 of footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,10 +1,73 @@
 import styled from "styled-components";
-import { Button, Image, Menu } from "semantic-ui-react";
+import { Image } from "semantic-ui-react";
 
 export const StyledLogo = styled(Image)`
   &&& {
     width: 30px;
-    margin: 0px 0px 0px 100px;
-    border: 0px;
+    margin: 20px 20px 20px 100px;
+    border: 95px;
+  }
+`;
+
+export const StyledText = styled.div`
+  &&& {
+    margin: 20px;
+    font-size: 16px;
+  }
+`;
+
+export const StyledFooterWrapper = styled.div`
+  &&& {
+    flex-shrink: 0;
+    text-align: center;
+    margin-top: 1rem;
+    padding: 1rem;
+    color: white;
+    background-color: #4a607b;
+    font-size: 16px;
+  }
+`;
+
+export const StyledEmail = styled.div`
+  &&& {
+    margin: 0px;
+    font-size: 26px;
+    text-align: center;
+  }
+`;
+
+export const StyledColumnWrapper = styled.div`
+  &&& {
+    margin: 16px;
+    padding: 0px 400px 20px;
+  }
+`;
+
+export const StyledRow = styled.div`
+  &&& {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 100%;
+    margin: 15px;
+  }
+`;
+
+export const StyledColumn = styled.div`
+  &&& {
+    display: flex;
+    flex-direction: column;
+    flex-basis: 100%;
+    flex: 1;
+    font-size: 12px;
+    height: 25px;
+    margin: 15px;
+  }
+`;
+
+export const StyledCopyright = styled.div`
+  &&& {
+    margin: 20px;
+    font-size: 12px;
   }
 `;
