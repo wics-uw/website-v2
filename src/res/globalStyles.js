4a2b64a4d3ea8befd91675a0e0a4b8c1d3b48ab4
lindaayangg <lindayang0928@hotmail.com>
2020-08-19 00:26:59 -0400

    WICS-5: Add Mission section card component

--- src/res/globalStyles.js
+++ src/res/globalStyles.js
@@ -1,12 +1,15 @@
+// This file is for the styles for some components that are being used globally
 import styled from "styled-components";
 
+// Styles for the body of the website
 export const StyledBodyWrapper = styled.div`
   &&& {
     margin: 0px 190px;
   }
 `;
 
-export const StyledSectionWrapper = styled.body`
+// Styles for title text of each large section / topic
+export const StyledSectionText = styled.h3`
   &&& {
     margin-top: 115px;
     font-family: Karla;
