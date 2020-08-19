379c6402ec2906d5fc5325b3dc5caf9fd4d33637
lindaayangg <lindayang0928@hotmail.com>
2020-08-18 23:04:19 -0400

    WICS-5: Add mission section and global styles

---
+++ src/res/missionStrings.js
@@ -0,0 +1,6 @@
+const missionStrings = {
+  ourMission: 'Our Mission',
+  missionDescription: 'WiCS is dedicated to promoting women who are interested in studying computer science and who seek to pursue careers in computing.',
+};
+
+export default missionStrings;
---
+++ src/sections/Mission/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const StyledMissionTitle = styled.h2`
+  &&& {
+    margin-top: 15px;
+    width: 185px;
+    font-family: "Karla Bold";
+    font-style: normal;
+    font-weight: bold;
+    font-size: 32px;
+    line-height: 37px;
+    text-transform: capitalize;
+    color: #000000;
+  }
+`;
+
+export const StyledMissionDescription = styled.h4`
+  &&& {
+    margin-top: 5px;
+    width: 648px;
+    font-family: Karla;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    line-height: 28px;
+    letter-spacing: -0.02em;
+    color: #000000;
+  }
+`;
