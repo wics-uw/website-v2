2fc6c5b4a3bca40383e989b259efbd1c437fa7ea
Jasmine Bal <jasminebal@Jasmines-MacBook-Pro.local>
2020-08-21 00:06:11 -0400

    added studentStrings

---
+++ src/res/studentStrings.js
@@ -0,0 +1,23 @@
+const strings = {
+    title: 'Student-Led Events',
+    description: 'Events and opportunities run by the students in the WiCS Undergraduate Committee.',
+    officeHours: {
+        title: 'Office Hours',
+        description: "Ut viverra dolor quam, at commodo ex accumsan vitae. Fusce libero ipsum, dictum sagittis dignissim eu, dictum sit amet leo. Vivamus a magna urna. Curabitur tincidunt nunc at risus viverra.",
+        mon: '4PM - 5PM',
+        tues: '4PM - 5PM',
+        wed: '4PM - 5PM',
+        thurs: '4PM - 5PM',
+        fri: '4PM - 5PM'
+    },
+    careerMentorship: {
+        title: 'Career & Mentorship',
+        description: 'Ut viverra dolor quam, at commodo ex accumsan vitae. Fusce libero ipsum, dictum sagittis dignissim eu, dictum sit amet leo. Vivamus a magna urna. Curabitur tincidunt nunc at risus viverra.'
+    },
+    community: {
+        title: 'Community Building',
+        description: 'Ut viverra dolor quam, at commodo ex accumsan vitae. Fusce libero ipsum, dictum sagittis dignissim eu, dictum sit amet leo. Vivamus a magna urna. Curabitur tincidunt nunc at risus viverra.'
+    }
+};
+
+export default strings;
