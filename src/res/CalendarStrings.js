b664047d83314daa26558c951414e258f2679ea6
Angela Zhang <angelazhangyr@gmail.com>
2020-08-24 00:22:26 -0400

    renamed file

--- src/res/CalendarStrings.js
+++ src/res/CalendarStrings.js
@@ -1,7 +1,7 @@
 const calendarStrings = {
-  calendarTitle: "Upcoming Events",
-  link: "https://calendar.google.com/calendar/embed?src=mfj5n653b9d95mvhj1ld9o2d30%40group.calendar.google.com&ctz=America%2FToronto",
-  loadingText: "Loading...",
-};
-
-export default calendarStrings;
+    calendarTitle: "Upcoming Events",
+    link: "https://calendar.google.com/calendar/embed?src=mfj5n653b9d95mvhj1ld9o2d30%40group.calendar.google.com&ctz=America%2FToronto",
+    loadingText: "Loading...",
+  };
+  
+  export default calendarStrings;
