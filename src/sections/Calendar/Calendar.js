ac87f12d08b2fa7a1e39e09b9d81bdc2cd725035
lindaayangg <lindayang0928@hotmail.com>
2020-08-25 01:03:32 -0400

    Change style

--- src/sections/Calendar/Calendar.js
+++ src/sections/Calendar/Calendar.js
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 import calendarStrings from "../../res/calendarStrings";
 import SectionTitleDescription from "../../components/SectionTitleDescription/SectionTitleDescription";
 import CalendarCard from "../../components/CalendarIframe/CalendarCard";
+import {StyledCalendarWrapper} from "./styles";
 
 class Calendar extends Component {
   render() {
     return (
       <div>
         <SectionTitleDescription title={calendarStrings.calendarTitle} />
-        <div class="ui hidden divider" />
-        <div className="Calendar">
+        <StyledCalendarWrapper>
           <CalendarCard source={calendarStrings.link} />
-        </div>
+        </StyledCalendarWrapper>
       </div>
     );
   }
