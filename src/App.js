7df7b729c5b89e6e39fb85765cdf8f66e4c919cc
Angela Zhang <angelazhangyr@gmail.com>
2020-08-27 20:54:08 -0400

    Deleted other sections

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Landing from "./sections/Landing/Landing";
 import Mission from "./sections/Mission/Mission";
 import Calendar from "./sections/Calendar/Calendar";
-import Structure from "./sections/Structure/Structure";
 import Numbers from "./sections/Numbers/Numbers";
-import FooterV2 from "./sections/Footer/Footer-v2";
 import {StyledBodyWrapper, StyledSectionText} from "./res/globalStyles";
 import strings from "./res/strings";
 import CurrentCommittee from "./sections/CurrentCommittee/CurrentCommittee";
@@ -23,7 +21,7 @@ function App() {
           {strings.about}
         </StyledSectionText>
         <Mission/>
-        <Structure />
+
         <Numbers/>
 
         <StyledSectionText id="calendar">
@@ -51,7 +49,7 @@ function App() {
        
        <Contact />
       </StyledBodyWrapper>
-      <FooterV2/>
+
     </div>
   );
 }
