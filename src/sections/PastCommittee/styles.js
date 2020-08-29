25aed61650da74000147b4677074cbc45e58fa65
lindaayangg <lindayang0928@hotmail.com>
2020-08-28 23:05:54 -0400

    Add Past committee v2

--- src/sections/PastCommittee/styles.js
+++ src/sections/PastCommittee/styles.js
@@ -1,17 +1,9 @@
 import styled from "styled-components";
-import {GridColumn, Icon} from "semantic-ui-react";
+import {Accordion, AccordionContent, AccordionTitle, Container, Grid, GridColumn, Image} from "semantic-ui-react";
 
-export const StyledPastExecsSectionWrapper = styled.div`
+export const StyledPastExecsSectionContainer = styled(Container)`
   &&& {
-    margin-top: 50px;
-    margin-bottom: 200px;
-  }
-`;
-
-export const StyledChevronIcon = styled(Icon)`
-  &&& {
-    margin-left: 31px;
-    margin-top: 20px;
+    width: 1060px;
   }
 `;
 
@@ -23,15 +15,61 @@ export const StyledImageWrapper = styled.div`
   }
 `;
 
+export const StyledImage = styled(Image)`
+  &&& {
+    width: 800px;
+  }
+`;
+
+export const StyledAccordion = styled(Accordion)`
+  &&& {
+    border-radius: 15px;
+    background: #FFFFFF;
+    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+  }
+`;
+
+export const StyledAccordionTitle = styled(AccordionTitle)`
+  &&& {
+    padding: 16px 30px 16px 26px !important;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`;
+
+export const StyledAccordionTitleText = styled.h4`
+  &&& {
+    font-family: Karla;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    line-height: 28px;
+    letter-spacing: -0.02em;
+    color: #000000;
+    margin: 0;
+  }
+`;
+
+export const StyledAccordionContent = styled(AccordionContent)`
+  &&& {
+    padding: 0 !important;
+    border-left: 3px solid #E7BEB2;
+    border-top: 1px solid rgba(0, 0, 0, 0.1);
+    background-color: rgba(231,190,178, 0.15);
+    border-radius: 0 0 15px 15px;
+  }
+`;
+
 export const StyledPastExecsTableWrapper = styled.div`
   &&& {
-    margin: 0 15px;
+    margin: 0 25px;
   }
 `;
 
 export const StyledTermWrapper = styled.div`
   &&& {
-    margin-top: 60px;
+    margin-top: 32px;
   }
 `;
 
@@ -47,9 +85,15 @@ export const StyledTermText = styled.h3`
   }
 `;
 
-export const StyledExecColumnWrapper = styled(GridColumn)`
+export const StyledGrid = styled(Grid)`
   &&& {
-    padding-top: 1rem !important;
-    padding-bottom: 0 !important;
+    margin-top: 16px;
+  }
+`;
+
+export const StyledGridColumn = styled(GridColumn)`
+  &&& {
+    padding-top: 0 !important;
+    padding-right: 0 !important;
   }
 `;
