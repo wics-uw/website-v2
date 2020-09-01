d29a877f65940bccafdc94f637ed24a216d3327a
Jasmine Bal <jasminebal@Jasmines-MacBook-Pro.local>
2020-09-01 01:14:39 -0400

    WiCS-9: adjusted x and altered strings

--- src/components/Modal1/Modal1.js
+++ src/components/Modal1/Modal1.js
@@ -1,31 +1,45 @@
 import React, { Component } from 'react';
-import {Modal, Header, Icon} from 'semantic-ui-react';
+import { Modal, Header, Icon, Button } from 'semantic-ui-react';
 import FacultyCard from '../FacultyCard/FacultyCard';
 import facultyStrings from "../../res/facultyStrings";
 import sponsored from '../../res/images/sponsored.svg';
-import {CloseStyled} from "./styles";
+import { CloseStyled, StyledHeader, HWrapper, StyledTitle, StyledDescription } from "./styles";
 
 export default function Modal1() {
+
+
+  const [open, setOpen] = React.useState(false)
+  return (
+    <div>
+      <Modal
+        open={open}
+        trigger={<div><FacultyCard title={facultyStrings.sponsored.title} image={sponsored} descriptionShort={facultyStrings.sponsored.description} /></div>}
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}>
+        <StyledHeader>
+          
+        <CloseStyled>
+        <Icon name='close' onClick= {() => setOpen(false)} color='black'/>
+        </CloseStyled>
+        <HWrapper>
+        <StyledTitle>
+        {facultyStrings.sponsored.title}
+        </StyledTitle>
+        <StyledDescription>
+        {facultyStrings.sponsored.description}
+        </StyledDescription>
+        </HWrapper>
+        
+        </StyledHeader>
+        <Modal.Content>
+          <CloseStyled icon='close'/>
+          <p>
+            {facultyStrings.sponsored.description}
+          </p>
     
-    
-        const [open, setOpen] = React.useState(false)
-        return (
-            <div>
-                <Modal
-                        open={open}
-                        trigger={<div><FacultyCard title={facultyStrings.sponsored.title} image={sponsored} descriptionShort={facultyStrings.sponsored.description}/></div>}
-                        onClose={() => setOpen(false)}
-                        onOpen={() => setOpen(true)}>
-                        <Header><Icon icon='close' color= 'black'/>{facultyStrings.sponsored.title}
-                        </Header> 
-                        <Modal.Content>
-                            <CloseStyled icon='close'/>
-                            <p>
-                            {facultyStrings.sponsored.description}
-                            </p>
-                        </Modal.Content>
-                </Modal>
-            </div>
-        )
-    
+        </Modal.Content>
+      </Modal>
+    </div>
+  )
+
 }
--- src/res/facultyStrings.js
+++ src/res/facultyStrings.js
@@ -1,17 +1,82 @@
 const facultyStrings = {
-    title: 'Faculty-Led Events',
-    description: 'Events and opportunities hosted by sponsors and the staff of the faculty-level WiCS Committee.',
+    title: 'Big WiCS Events',
+    description: 'Events and opportunities hosted by faculty and staff of the at-large committee and sponsors.',
     sponsored: {
-        title: 'Sponsored Events',
-        description: 'Info sessions and events held by our sponsors and partners, including Facebook, Google and more!'
+        title: 'Sponsor Events',
+        description: 'Info sessions and events held by our sponsors and partners, including Facebook, Google and more!',
+        popupHead: 'Past Sponsor Events included:',
+        section1: 'Professional Development Sessions',
+        s1List: {
+            l1: 'Technical Interview Tips',
+            l2:" Ace Your Technical Interview with Amazon",
+            l3: 'Resume Critiques with Google',
+            l4: 'Mock Interviews',
+            l5: 'Make the Most of Your Co-op with Flexport'
+        },
+        section2: 'Personal Development Sessions',
+        s2List: {
+            l1: 'Know Your Worth with Splunk',
+            l2: 'Unconscious Bias: How to Recognize and Interrupt It',
+            l3: 'How to Network for Introverts',
+            l4: 'Sexism Response Workshop'
+        },
+        section3: 'Sponsor Events',
+        s3List: {
+            l1: 'Salesforce Dinners',
+            l2: 'Datadog: Why is Monitoring Important?',
+            l3: 'Shopify Developers Panel',
+            l4: 'Technology Circles with Facebook',
+            l5: 'Facebook Puzzle Hunt'
+        }
     },
     tech:{
         title: 'Tech Confrence',
-        description: 'Event to hash out new ideas with friends and make memories! more words etc etc'
+        description: 'Event to hash out new ideas with friends and make memories! more words etc etc',
+        popupHead: 'These Opportunities Include:',
+        section1: 'Professional Development Sessions',
+        s1List: {
+            l1: 'Technical Interview Tips',
+            l2:" Ace Your Technical Interview with Amazon",
+            l3: 'Resume Critiques with Google',
+            l4: 'Mock Interviews',
+            l5: 'Make the Most of Your Co-op with Flexport'
+        },
+        section2: 'Personal Development Sessions',
+        s2List: {
+            l1: 'Know Your Worth with Splunk',
+            l2: 'Unconscious Bias: How to Recognize and Interrupt It',
+            l3: 'How to Network for Introverts',
+            l4: 'Sexism Response Workshop'
+        },
+        section3: 'Sponsor Events',
+        s3List: {
+            l1: 'Salesforce Dinners',
+            l2: 'Datadog: Why is Monitoring Important?',
+            l3: 'Shopify Developers Panel',
+            l4: 'Technology Circles with Facebook',
+            l5: 'Facebook Puzzle Hunt'
+        }
     },
     outreach: {
         title: 'Youth Outreach',
-        description: 'Ways we try to interact with high school students etc technovation etc more words go here'
+        description: 'Ways we try to interact with high school students etc technovation etc more words go here',
+        popupHead: 'Youth Outreach',
+        popupLink: 'https://cs.uwaterloo.ca/wics/wics-outreach',
+        section1: 'GIRLsmarts4tech',
+        s1Link: 'https://cs.uwaterloo.ca/wics/events-activities/girlsmarts4tech',
+        s1List: {
+            l1: 'a free outreach program aimed at inspiring girls to explore technology. During the day-long workshop, girls will learn about various aspects of computer science, including mobile app design and programming, user interfaces, and how computer science applies in other fields and disciplines.',
+        },
+        section2: 'Technovation Girls',
+        s2Link: 'https://cs.uwaterloo.ca/wics/events-and-activities-girls-offered-wics/technovation-girls',
+        s2List: {
+            l1: 'Know Your Worth with Splunk',
+        },
+        section3: 'Girls Mean Business',
+        s3Link: '',
+        s3List: {
+            l1: 'a series of hands-on programs for girls in grades 6 through 12 that guide the girls through the lifecycle of becoming a technology entrepreneur. Topics include ideation, user interface design, coding a mobile application, cybersecurity, e-commerce, digital media marketing, and business planning.',
+        }
     }
 };
 
