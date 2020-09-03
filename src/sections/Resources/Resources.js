33d73ab3371537c968bdad323a44f6364a8bcd1d
lindaayangg <lindayang0928@hotmail.com>
2020-09-03 16:39:46 -0400

    Create accordion component for resources

--- src/sections/Resources/Resources.js
+++ src/sections/Resources/Resources.js
@@ -2,33 +2,16 @@ import React, {Component} from 'react';
 import SectionTitleDescription from '../../components/SectionTitleDescription/SectionTitleDescription';
 import resourcesStrings from '../../res/strings/resourcesStrings';
 import {Grid, GridColumn} from 'semantic-ui-react';
-import {StyledAccordion, StyledAccordionContent, StyledAccordionContentR, StyledAccordionTitle, StyledATitle} from './styles';
+import {StyledAccordion} from './styles';
 import {StyledSectionText, StyledSubSectionWrapper} from '../../res/globalStyles';
 import resourceImg from "../../res/images/resource.svg";
 import Image from "semantic-ui-react/dist/commonjs/elements/Image";
 import strings from "../../res/strings/strings";
-import chevron_down from "../../res/images/past_execs_chevron_down.svg";
-import chevron_right from "../../res/images/past_execs_chevron_right.svg";
-import ResourcesAccordionContent from "../../components/ResourcesAccordionContent/ResourcesAccordionContent";
+import ResourcesAccordion from "../../components/ResourcesAccordion/ResourcesAccordion";
 
 //resources component
 export default class Resources extends Component {
-
-  //functions to control state
-  state = {activeIndex: -1};
-
-  handleClick = (e, titleProps) => {
-    const {index} = titleProps;
-    const {activeIndex} = this.state;
-    const newIndex = activeIndex === index ? -1 : index;
-
-    this.setState({activeIndex: newIndex})
-  };
-
   render() {
-    //to help control state
-    const {activeIndex} = this.state;
-
     return (
       <StyledSubSectionWrapper>
         {/*the accordion */}
@@ -37,84 +20,23 @@ export default class Resources extends Component {
             <StyledSectionText>{strings.resources}</StyledSectionText>
             <SectionTitleDescription title={resourcesStrings.title} description=''/>
             <StyledAccordion fluid styled>
-              {/*first section of accordion */}
-              {/*first accordion title */}
-              <StyledAccordionTitle
-                active={activeIndex === 0}
+              <ResourcesAccordion
                 index={0}
-                onClick={this.handleClick}>
-                <StyledATitle>
-                  {resourcesStrings.wicsTitle}
-                </StyledATitle>
-                {activeIndex === 0 ? <Image src={chevron_down}/> : <Image src={chevron_right}/>}
-              </StyledAccordionTitle>
-
-              {/*first accordion content */}
-              <StyledAccordionContent active={activeIndex === 0}>
-                {Object.entries(resourcesStrings.wicsLinks).map(([number, resource]) => (
-                  <ResourcesAccordionContent
-                    link={resource.link}
-                    linkName={resource.name}/>))}
-              </StyledAccordionContent>
-
-              {/*second section of accordion */}
-              {/*second accordion title */}
-              <StyledAccordionTitle
-                active={activeIndex === 1}
+                title={resourcesStrings.wicsTitle}
+                content={resourcesStrings.wicsLinks}/>
+              <ResourcesAccordion
                 index={1}
-                onClick={this.handleClick}>
-                <StyledATitle>
-                  {resourcesStrings.conferenceTitle}
-                </StyledATitle>
-                {activeIndex === 1 ? <Image src={chevron_down}/> : <Image src={chevron_right}/>}
-              </StyledAccordionTitle>
-
-              {/*second accordion content */}
-              <StyledAccordionContent active={activeIndex === 1}>
-                {Object.entries(resourcesStrings.conferenceLinks).map(([number, resource]) => (
-                  <ResourcesAccordionContent
-                    link={resource.link}
-                    linkName={resource.name}/>))}
-              </StyledAccordionContent>
-
-              {/*third section of accordion */}
-              {/*third accordion title */}
-              <StyledAccordionTitle
-                active={activeIndex === 2}
+                title={resourcesStrings.conferenceTitle}
+                content={resourcesStrings.conferenceLinks}/>
+              <ResourcesAccordion
                 index={2}
-                onClick={this.handleClick}>
-                <StyledATitle>
-                  {resourcesStrings.additionalTitle}
-                </StyledATitle>
-                {activeIndex === 2 ? <Image src={chevron_down}/> : <Image src={chevron_right}/>}
-              </StyledAccordionTitle>
-              {/*third accordion content */}
-              <StyledAccordionContent active={activeIndex === 2}>
-                {Object.entries(resourcesStrings.additionLinks).map(([number, resource]) => (
-                  <ResourcesAccordionContent
-                    link={resource.link}
-                    linkName={resource.name}/>))}
-              </StyledAccordionContent>
-
-              {/*fourth section of accordion */}
-              {/*fourth accordion title */}
-              <StyledAccordionTitle
-                active={activeIndex === 3}
+                title={resourcesStrings.additionalTitle}
+                content={resourcesStrings.additionLinks}/>
+              <ResourcesAccordion
                 index={3}
-                onClick={this.handleClick}>
-                <StyledATitle>
-                  {resourcesStrings.womenTitle}
-                </StyledATitle>
-                {activeIndex === 3 ? <Image src={chevron_down}/> : <Image src={chevron_right}/>}
-              </StyledAccordionTitle>
-
-              {/*fourth accordion content */}
-              <StyledAccordionContentR active={activeIndex === 3}>
-                {Object.entries(resourcesStrings.womenLinks).map(([number, resource]) => (
-                  <ResourcesAccordionContent
-                    link={resource.link}
-                    linkName={resource.name}/>))}
-              </StyledAccordionContentR>
+                last={true}
+                title={resourcesStrings.womenTitle}
+                content={resourcesStrings.womenLinks}/>
             </StyledAccordion>
           </GridColumn>
           <GridColumn width={9}>
--- src/sections/Resources/styles.js
+++ src/sections/Resources/styles.js
@@ -1,19 +1,5 @@
 import styled from 'styled-components';
-import {Accordion, AccordionContent, AccordionTitle} from 'semantic-ui-react';
-
-//styles for the text in the Accordion title
-export const StyledATitle = styled.h4`
-  &&& {
-    font-family: Karla;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 24px;
-    line-height: 28px;
-    letter-spacing: -0.02em;
-    color: #000000;
-    margin: 0;
-  }
-`;
+import {Accordion} from 'semantic-ui-react';
 
 //styles for the accordion
 export const StyledAccordion = styled(Accordion)`
@@ -21,35 +7,5 @@ export const StyledAccordion = styled(Accordion)`
     border-radius: 15px;
     background: #FFFFFF;
     box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
-  }
-`;
-
-export const StyledAccordionTitle = styled(AccordionTitle)`
-  &&& {
-    padding: 16px 24px 16px 16px !important;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-  }
-`;
-
-//styles for the accordion content
-export const StyledAccordionContentR = styled(AccordionContent)`
-  &&& {
-    padding: 16px 24px !important;
-    border-left: 3px solid #E7BEB2;
-    border-top: 1px solid rgba(0, 0, 0, 0.1);
-    background-color: rgba(231,190,178, 0.15);
-    border-radius: 0 0 15px 15px;
-  }
-`;
-
-//styles for the accordion content
-export const StyledAccordionContent = styled(AccordionContent)`
-  &&& {
-    padding: 16px 24px !important;
-    border-left: 3px solid #E7BEB2;
-    border-top: 1px solid rgba(0, 0, 0, 0.1);
-    background-color: rgba(231,190,178, 0.15);
   }
 `;
