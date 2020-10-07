34a28efc32d4bea2107db4103061c155975f5014
Celine Lee <celineselee@gmail.com>
2020-10-07 14:22:58 -0400

    update calendar src link

--- src/res/strings/calendarStrings.js
+++ src/res/strings/calendarStrings.js
@@ -1,6 +1,6 @@
 const calendarStrings = {
   calendarTitle: "Monthly Schedule",
-  link: "https://calendar.google.com/calendar/embed?src=0kst6i6n49krq8kp4qbkh23ic8%40group.calendar.google.com&ctz=America%2FToronto",
+  link: "https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FToronto&amp;src=NHBnZjBkbjI4OGJtNnBxMHNrdm5tdHAxdHNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=bWZqNW42NTNiOWQ5NW12aGoxbGQ5bzJkMzBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23c3c3c3&amp;color=%23D81B60&amp;showTitle=1&amp;title=WiCS%20Events!&amp;showTz=1&amp;showPrint=0",
   loadingText: "Loading...",
   view: "View Google Calendar"
 };
