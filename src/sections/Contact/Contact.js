24115dee4ea61aea9911ec49a19c8d02363b0e62
mollyxu <64995721+mollyxu@users.noreply.github.com>
2021-02-12 22:25:59 -0800

    added discord to landing page, socials, and footer

--- src/sections/Contact/Contact.js
+++ src/sections/Contact/Contact.js
@@ -1,6 +1,6 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import contactStrings from "../../res/strings/contactStrings";
-import {Grid, Image, ImageGroup} from "semantic-ui-react";
+import { Grid, Image, ImageGroup } from "semantic-ui-react";
 import {
   StyledFaqLink,
   StyledReachOutLink,
@@ -16,17 +16,23 @@ import linkedin from "../../res/images/socials/inactive/linkedin-dark.svg";
 import facebook from "../../res/images/socials/inactive/facebook-dark.svg";
 import slack from "../../res/images/socials/inactive/slack-dark.svg";
 import instagram from "../../res/images/socials/inactive/ig-dark.svg";
-import {StyledSectionText, StyledSocialLink, StyledSubSectionWrapper} from "../../res/globalStyles";
+import discord from "../../res/images/socials/inactive/discord-dark.svg";
+import {
+  StyledSectionText,
+  StyledSocialLink,
+  StyledSubSectionWrapper,
+} from "../../res/globalStyles";
 import strings from "../../res/strings/strings";
 import linkedin_hover from "../../res/images/socials/hover/linkedin-dark.svg";
 import facebook_hover from "../../res/images/socials/hover/facebook-dark.svg";
 import slack_hover from "../../res/images/socials/hover/slack-dark.svg";
 import instagram_hover from "../../res/images/socials/hover/ig-dark.svg";
+import discord_hover from "../../res/images/socials/hover/discord-dark.svg";
 
 class Contact extends Component {
   constructor(props) {
     super(props);
-    this.state = {screenWidth: null};
+    this.state = { screenWidth: null };
     this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
   }
 
@@ -35,77 +41,113 @@ class Contact extends Component {
   }
 
   componentWillUnmount() {
-    window.removeEventListener("resize", this.updateWindowDimensions)
+    window.removeEventListener("resize", this.updateWindowDimensions);
   }
 
   updateWindowDimensions() {
-    this.setState({screenWidth: window.innerWidth});
+    this.setState({ screenWidth: window.innerWidth });
   }
 
   renderContent() {
-    return <>
-      <StyledSectionText>{strings.contactUs}</StyledSectionText>
-      <StyledSubContactSectionWrapper>
-        <StyledTitle>{contactStrings.faq.title}</StyledTitle>
-        <StyledText>
-          {contactStrings.faq.d1}
-          <StyledFaqLink href={contactStrings.faq.faqlink} target='_blank'>
-            {contactStrings.faq.faqtext}
-          </StyledFaqLink>
-          {contactStrings.faq.d2}
-          <StyledFaqLink href={contactStrings.faq.policylink} target='_blank'>
-            {contactStrings.faq.policytext}
-          </StyledFaqLink>
-        </StyledText>
-      </StyledSubContactSectionWrapper>
+    return (
+      <>
+        <StyledSectionText>{strings.contactUs}</StyledSectionText>
+        <StyledSubContactSectionWrapper>
+          <StyledTitle>{contactStrings.faq.title}</StyledTitle>
+          <StyledText>
+            {contactStrings.faq.d1}
+            <StyledFaqLink href={contactStrings.faq.faqlink} target="_blank">
+              {contactStrings.faq.faqtext}
+            </StyledFaqLink>
+            {contactStrings.faq.d2}
+            <StyledFaqLink href={contactStrings.faq.policylink} target="_blank">
+              {contactStrings.faq.policytext}
+            </StyledFaqLink>
+          </StyledText>
+        </StyledSubContactSectionWrapper>
 
-      <StyledSubContactSectionWrapper2>
-        <StyledTitle>{contactStrings.ro.title}</StyledTitle>
-        <StyledText>
-          {contactStrings.ro.d1}<br/>
-          {contactStrings.ro.d2}
-          <StyledReachOutLink href={strings.social.emailLink}
-                              target="_blank">{contactStrings.ro.email}</StyledReachOutLink>
-        </StyledText>
-      </StyledSubContactSectionWrapper2>
+        <StyledSubContactSectionWrapper2>
+          <StyledTitle>{contactStrings.ro.title}</StyledTitle>
+          <StyledText>
+            {contactStrings.ro.d1}
+            <br />
+            {contactStrings.ro.d2}
+            <StyledReachOutLink href={strings.social.emailLink} target="_blank">
+              {contactStrings.ro.email}
+            </StyledReachOutLink>
+          </StyledText>
+        </StyledSubContactSectionWrapper2>
 
-      <StyledTitle2>{contactStrings.desc}</StyledTitle2>
-      <ImageGroup>
-        <StyledSocialLink href={strings.social.linkedin} target="_blank" rel="noopener noreferrer">
-          <img
-            alt=""
-            src={linkedin}
-            onMouseOver={e => (e.currentTarget.src = linkedin_hover)}
-            onMouseOut={e => (e.currentTarget.src = linkedin)}/>
-        </StyledSocialLink>
-        <StyledSocialLink href={strings.social.facebook} target="_blank" rel="noopener noreferrer">
-          <img
-            alt=""
-            src={facebook}
-            onMouseOver={e => (e.currentTarget.src = facebook_hover)}
-            onMouseOut={e => (e.currentTarget.src = facebook)}
-          />
-        </StyledSocialLink>
-        <StyledSocialLink href={strings.social.instagram} target="_blank" rel="noopener noreferrer">
-          <img
-            alt=""
-            src={instagram}
-            onMouseOver={e => (e.currentTarget.src = instagram_hover)}
-            onMouseOut={e => (e.currentTarget.src = instagram)}/>
-        </StyledSocialLink>
-        <StyledSocialLink href={strings.social.slack} target="_blank" rel="noopener noreferrer">
-          <img
-            alt=""
-            src={slack}
-            onMouseOver={e => (e.currentTarget.src = slack_hover)}
-            onMouseOut={e => (e.currentTarget.src = slack)}/>
-        </StyledSocialLink>
-      </ImageGroup>
-    </>;
+        <StyledTitle2>{contactStrings.desc}</StyledTitle2>
+        <ImageGroup>
+          <StyledSocialLink
+            href={strings.social.linkedin}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              alt=""
+              src={linkedin}
+              onMouseOver={(e) => (e.currentTarget.src = linkedin_hover)}
+              onMouseOut={(e) => (e.currentTarget.src = linkedin)}
+            />
+          </StyledSocialLink>
+          <StyledSocialLink
+            href={strings.social.facebook}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              alt=""
+              src={facebook}
+              onMouseOver={(e) => (e.currentTarget.src = facebook_hover)}
+              onMouseOut={(e) => (e.currentTarget.src = facebook)}
+            />
+          </StyledSocialLink>
+          <StyledSocialLink
+            href={strings.social.instagram}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              alt=""
+              src={instagram}
+              onMouseOver={(e) => (e.currentTarget.src = instagram_hover)}
+              onMouseOut={(e) => (e.currentTarget.src = instagram)}
+            />
+          </StyledSocialLink>
+          <StyledSocialLink
+            href={strings.social.slack}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              alt=""
+              src={slack}
+              onMouseOver={(e) => (e.currentTarget.src = slack_hover)}
+              onMouseOut={(e) => (e.currentTarget.src = slack)}
+            />
+          </StyledSocialLink>
+
+          <StyledSocialLink
+            href={strings.social.discord}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              alt=""
+              src={discord}
+              onMouseOver={(e) => (e.currentTarget.src = discord_hover)}
+              onMouseOut={(e) => (e.currentTarget.src = discord)}
+            />
+          </StyledSocialLink>
+        </ImageGroup>
+      </>
+    );
   }
 
   render() {
-    const {screenWidth} = this.state;
+    const { screenWidth } = this.state;
 
     return (
       <StyledSubSectionWrapper>
@@ -113,11 +155,13 @@ class Contact extends Component {
           <Grid.Column computer={10} mobile={16}>
             {this.renderContent()}
           </Grid.Column>
-          {screenWidth <= 425
-            ? <></>
-            : <Grid.Column computer={6}>
-              <Image src={illustration} size="huge"/>
-            </Grid.Column>}
+          {screenWidth <= 425 ? (
+            <></>
+          ) : (
+            <Grid.Column computer={6}>
+              <Image src={illustration} size="huge" />
+            </Grid.Column>
+          )}
         </Grid>
       </StyledSubSectionWrapper>
     );
