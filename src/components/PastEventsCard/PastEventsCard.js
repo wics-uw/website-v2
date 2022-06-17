6dbec30b91514773b008539bbd8050bdc12f7a8d
Leen Kamalmaz <leenkamalmaz@gmail.com>
2022-06-17 01:10:17 -0400

    added photo descriptions to carousel

--- src/components/PastEventsCard/PastEventsCard.js
+++ src/components/PastEventsCard/PastEventsCard.js
@@ -1,12 +1,21 @@
 import Carousel from "react-multi-carousel";
 import { Image } from "semantic-ui-react";
 import React from "react";
+import styles from "../../sections/PastEvents/style.css";
 
 import pic1 from "../../res/images/carousel/W20_BOT2.jpg";
 import pic2 from "../../res/images/carousel/Club_Fair.jpg";
 import pic3 from "../../res/images/carousel/Technovation_EOT2.jpg";
 import pic4 from "../../res/images/carousel/Technovation_EOT.jpg";
 import pic5 from "../../res/images/carousel/W22_Exec_Social.jpg";
+
+const elements = [
+  [pic1, "Winter 2020 Beginning of Term Event"],
+  [pic2, "WiCS Booth at the Clubs Fair"],
+  [pic3, "Technovation End of Term Celebration"],
+  [pic4, "Technovation End of Term Celebration"],
+  [pic5, "Winter 2022 Exec Social"],
+];
 
 const responsive = {
   desktop: {
@@ -25,13 +34,6 @@ const responsive = {
     paritialVisibilityGutter: -0.5
   }
 };
-const images = [
-  pic1,
-  pic2,
-  pic3,
-  pic4,
-  pic5,
-];
 
 const PastEventsCard = ({ deviceType }) => {
   return (
@@ -42,15 +44,22 @@ const PastEventsCard = ({ deviceType }) => {
       itemClass="image-item"
       responsive={responsive}
     >
-      {images.slice(0, 5).map(image => {
+
+      {elements.map(element => {
         return (
-          <Image
-            draggable={false}
-            style={{ width: "100%", height: "100%", borderRadius: "10px"}}
-            src={image}
-          />
+          <div>
+            <Image
+              draggable={false}
+              style={{ width: "100%", height: "100%", borderRadius: "10px"}}
+              src={element[0]}
+            />
+            <div>
+              <div id="desc-text">{element[1]}</div>
+            </div>
+          </div>
         );
       })}
+
     </Carousel>
   );
 };
