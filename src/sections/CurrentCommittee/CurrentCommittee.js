f48aa0bc90e08c7ff7041648ac941561629a5499
shriya-31 <kaisthas31@gmail.com>
2023-05-31 21:54:59 -0400

    Updating current execs for S23

--- src/sections/CurrentCommittee/CurrentCommittee.js
+++ src/sections/CurrentCommittee/CurrentCommittee.js
@@ -1,29 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import SectionTitleDescription from "../../components/SectionTitleDescription/SectionTitleDescription";
 import currentExecStrings from "../../res/strings/currentExecStrings";
 
 // current committee member pictures
-import kitty from "../../res/images/w23execs/kitty.jpg";
-import amanda from "../../res/images/w23execs/amanda.jpg";
+import leen from "../../res/images/s23execs/leen.jpeg";
+import melanie from "../../res/images/s23execs/melanie.jpg";
 import veronica from "../../res/images/w23execs/veronica.jpg";
-import audrey from "../../res/images/w23execs/audrey.jpg";
-import yani from "../../res/images/w23execs/yani.jpg";
-import layne from "../../res/images/w23execs/layne.jpg";
-import shriya from "../../res/images/w23execs/shriya.jpg";
-import karneet from "../../res/images/w23execs/karneet.jpg";
-import prasanna from "../../res/images/w23execs/prasanna.jpg";
-import helena from "../../res/images/w23execs/helena.png";
+import priya from "../../res/images/s23execs/priya_saha.jpg";
+import shriya from "../../res/images/s23execs/shriya.jpg";
+import jemima from "../../res/images/s23execs/jemima.jpg";
+import valerie from "../../res/images/s23execs/valerie.jpg";
+import madeline from "../../res/images/s23execs/madeline.jpeg";
+import padena from "../../res/images/s23execs/padena.JPG";
+import iris from "../../res/images/s23execs/iris.jpg";
+import madison from "../../res/images/s23execs/madison.jpg";
 
 import CurrentCommitteeCard from "../../components/CurrentCommitteeCard/CurrentCommitteeCard";
-import {GridColumn} from "semantic-ui-react";
-import {StyledCurrExecSectionWrapper, StyledExecWrapper, StyledGridColumn, StyledMobileTerm} from "./styles";
+import { GridColumn } from "semantic-ui-react";
+import {
+  StyledCurrExecSectionWrapper,
+  StyledExecWrapper,
+  StyledGridColumn,
+  StyledMobileTerm,
+} from "./styles";
 
 // Component for current executives section
 class CurrentCommittee extends Component {
-
   constructor(props) {
     super(props);
-    this.state = {screenWidth: null};
+    this.state = { screenWidth: null };
     this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
   }
 
@@ -32,101 +37,104 @@ class CurrentCommittee extends Component {
   }
 
   componentWillUnmount() {
-    window.removeEventListener("resize", this.updateWindowDimensions)
+    window.removeEventListener("resize", this.updateWindowDimensions);
   }
 
   updateWindowDimensions() {
-    this.setState({screenWidth: window.innerWidth});
+    this.setState({ screenWidth: window.innerWidth });
   }
-
 
   renderCard = (picture, name, position) => {
     return (
-      <CurrentCommitteeCard
-        picture={picture}
-        name={name}
-        position={position}/>
-    )
+      <CurrentCommitteeCard picture={picture} name={name} position={position} />
+    );
   };
 
   renderComputerSize(executives) {
-    return <StyledExecWrapper columns={4}>
-      {Object.entries(executives).map(([key, execInfo]) => (
-        <GridColumn>
-          {this.renderCard(execInfo.pic, execInfo.name, execInfo.position)}
-        </GridColumn>
-      ))}
-    </StyledExecWrapper>;
+    return (
+      <StyledExecWrapper columns={4}>
+        {Object.entries(executives).map(([key, execInfo]) => (
+          <GridColumn>
+            {this.renderCard(execInfo.pic, execInfo.name, execInfo.position)}
+          </GridColumn>
+        ))}
+      </StyledExecWrapper>
+    );
   }
 
   renderMobileSize(executives) {
-    return <div>
-      <StyledMobileTerm>
-        {currentExecStrings.term}
-      </StyledMobileTerm>
-      <StyledExecWrapper columns={2}>
-        {Object.entries(executives).map(([key, execInfo]) => (
-          <StyledGridColumn>
-            {this.renderCard(execInfo.pic, execInfo.name, execInfo.position)}
-          </StyledGridColumn>
-        ))}
-      </StyledExecWrapper>
-    </div>;
+    return (
+      <div>
+        <StyledMobileTerm>{currentExecStrings.term}</StyledMobileTerm>
+        <StyledExecWrapper columns={2}>
+          {Object.entries(executives).map(([key, execInfo]) => (
+            <StyledGridColumn>
+              {this.renderCard(execInfo.pic, execInfo.name, execInfo.position)}
+            </StyledGridColumn>
+          ))}
+        </StyledExecWrapper>
+      </div>
+    );
   }
 
   render() {
-    const {screenWidth} = this.state;
+    const { screenWidth } = this.state;
     // Change current executives' images, names, and positions here
     const executives = {
       exec1: {
-        pic: kitty,
-        name: "Kitty Li",
-        position: "Co-chair"
+        pic: leen,
+        name: "Leen Kamalmaz",
+        position: "Co-chair",
       },
       exec2: {
-        pic: amanda,
-        name: "Amanda Bishop",
-        position: "Co-chair"
+        pic: melanie,
+        name: "Melanie Foltak",
+        position: "Co-chair",
       },
       exec3: {
         pic: veronica,
         name: "Veronika Sustrova",
-        position: "Secretary"
+        position: "Secretary",
       },
       exec4: {
-        pic: audrey,
-        name: "Audrey Ho",
-        position: "Director of Internal Affairs"
+        pic: iris,
+        name: "Iris Chen",
+        position: "Director of Internal Affairs",
       },
       exec5: {
-        pic: yani,
-        name: "Yani Mei",
-        position: "Systems Administrator"
-      },
-      exec6: {
-        pic: layne,
-        name: "Layne Lim Ah Tock",
-        position: "Director of Finance"
-      },
-      exec7: {
         pic: shriya,
         name: "Shriya Kaistha",
-        position: "Director of Pubilicity"
+        position: "Systems Administrator",
+      },
+      exec6: {
+        pic: priya,
+        name: "Priyadarshini Saha",
+        position: "Director of Finance",
+      },
+      exec7: {
+        pic: valerie,
+        name: "Valerie Fernandes",
+        position: "Co-Director of Publicity",
       },
       exec8: {
-        pic: karneet,
-        name: "Karneet Kaur",
-        position: "Director of Outreach"
+        pic: madison,
+        name: "Madison Mussari",
+        position: "Co-Director of Publicity",
       },
       exec9: {
-        pic: prasanna,
-        name: "Prasanna Thallapalli",
-        position: "Operations Manager"
+        pic: jemima,
+        name: "Jemima Vijayasenan",
+        position: "Director of Outreach",
       },
       exec10: {
-        pic: helena,
-        name: "Helena Xu",
-        position: "First Year Representative"
+        pic: padena,
+        name: "Padena Rasouli-Baghban",
+        position: "Operations Manager",
+      },
+      exec11: {
+        pic: madeline,
+        name: "Madeline Pinto",
+        position: "First Year Representative",
       },
     };
 
@@ -134,13 +142,14 @@ class CurrentCommittee extends Component {
       <StyledCurrExecSectionWrapper>
         <SectionTitleDescription
           title={currentExecStrings.title}
-          description={screenWidth <= 425 ? '' : currentExecStrings.term}
+          description={screenWidth <= 425 ? "" : currentExecStrings.term}
         />
-        {screenWidth <= 425 ? this.renderMobileSize(executives) : this.renderComputerSize(executives)}
+        {screenWidth <= 425
+          ? this.renderMobileSize(executives)
+          : this.renderComputerSize(executives)}
       </StyledCurrExecSectionWrapper>
-    )
+    );
   }
-
 }
 
 export default CurrentCommittee;
