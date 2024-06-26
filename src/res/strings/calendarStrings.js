820ffebd94d28fcfb01ccc9741d2a939b106f0c0
Dami Adeyemo <ooadeyem@uwaterloo.ca>
2024-06-26 17:03:11 -0400

    Updated pictures and style for carousel + calendar.

--- src/res/strings/calendarStrings.js
+++ src/res/strings/calendarStrings.js
@@ -1,6 +1,6 @@
 const calendarStrings = {
   calendarTitle: "Monthly Schedule",
-  link: "https://calendar.google.com/calendar/embed?src=0kst6i6n49krq8kp4qbkh23ic8%40group.calendar.google.com&ctz=America%2FToronto",
+  link: "https://calendar.google.com/calendar/embed?src=q3p0qg5mbi4uqrf1p0o1rpdhb4ep4m4o%40import.calendar.google.com&ctz=America%2FToronto",
   loadingText: "Loading...",
   view: "View Google Calendar"
 };
