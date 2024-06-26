a4fa1e1b1e5b30e92266ae22adc9e974a0f719d3
Dami Adeyemo <ooadeyem@uwaterloo.ca>
2024-06-26 17:40:15 -0400

    Fixing bug.

--- src/res/strings/calendarStrings.js
+++ src/res/strings/calendarStrings.js
@@ -1,6 +1,6 @@
 const calendarStrings = {
   calendarTitle: "Monthly Schedule",
-  link: "https://calendar.google.com/calendar/embed?src=q3p0qg5mbi4uqrf1p0o1rpdhb4ep4m4o%40import.calendar.google.com&ctz=America%2FToronto",
+  link: "https://calendar.google.com/calendar/u/2?cid=MGtzdDZpNm40OWtycThrcDRxYmtoMjNpYzhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
   loadingText: "Loading...",
   view: "View Google Calendar"
 };
